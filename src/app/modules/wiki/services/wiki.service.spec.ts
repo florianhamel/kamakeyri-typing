edc7375012681f966eeecf55166bc6013a1eee11
Florian Hamel <fhamel.freelance@gmail.com>
2024-11-11 11:51:11 +0100

    💾 install: ⚠️ upgraded to angula18 with issues at npm install

--- src/app/modules/wiki/services/wiki.service.spec.ts
+++ src/app/modules/wiki/services/wiki.service.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WikiService } from './wiki.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('WikiService', () => {
   let service: WikiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    });
+    imports: [],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+});
     service = TestBed.inject(WikiService);
   });
 
