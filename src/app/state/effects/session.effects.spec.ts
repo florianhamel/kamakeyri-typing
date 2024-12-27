241d5432fe3907efb97396ad9e7f7ae76250417d
Florian Hamel <fhamel.freelance@gmail.com>
2024-12-27 14:17:52 +0100

    🔨 fixed tests

--- src/app/state/effects/session.effects.spec.ts
+++ src/app/state/effects/session.effects.spec.ts
@@ -1,52 +1,57 @@
-import { sessionUploadOrSave, sessionUploadAllSaved } from '../../store/session.effects';
 import { of, throwError } from 'rxjs';
-import { sessionActions } from '../../store/session.actions';
-import { SessionInfo, SessionMetaData, SessionRefined } from '../../models/session.types';
 import { TestBed } from '@angular/core/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { initialState } from '../../store/session.state';
-import { SessionService } from '../../services/session.service';
-import { selectSessionRefined } from '../../store/session.selectors';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { MockSessionStorageService } from '../../../../mocks/mock-session-storage.service';
-import { clearSessionItems, getSessionItem, setSessionItem } from '../../../../common/helpers';
-import { selectIsLoggedIn } from '../../../auth/store/auth.selectors';
-import { generateMock } from '../../../../mocks/mocking.tools';
-import { generateSessionDto, generateSessionRefined } from '../../../../mocks/factories.tools';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { generateSessionDto, generateSessionData } from '../../application/mocks/factories.tools';
+import { SessionService } from '../../application/services/session.service';
+import { generateMock } from '../../application/mocks/mocking.tools';
+import { initialState } from '../states/session.state';
+import { selectIsLoggedIn } from '../selectors/auth.selectors';
+import { MockSessionStorageService } from '../../application/mocks/mock-session-storage.service';
+import { Session, SessionData, SessionMetaData } from '../../domain/types/session.types';
+import { selectSessionData } from '../selectors/session.selectors';
+import { clearSessionItems, getSessionItem, setSessionItem } from '../../application/helpers/storage.helper';
+import { sessionActions } from '../actions/session.actions';
+import { SessionMode } from '../../domain/enums/session-mode.enum';
+import { SessionOption } from '../../domain/enums/session-option.enum';
+import { sessionUploadAllSaved, sessionUploadOrSave } from './session.effects';
 
 describe('session effects', () => {
-  const sessionRefined: SessionRefined = generateSessionRefined();
-  const sessionServiceMock: jest.Mocked<SessionService> =
-    generateMock<SessionService>('uploadSessions');
+  const sessionRefined: SessionData = generateSessionData();
+  const sessionServiceMock: jest.Mocked<SessionService> = generateMock<SessionService>('uploadSessions');
   let mockStore: MockStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [],
-    providers: [
+      imports: [],
+      providers: [
         provideMockStore({
-            initialState,
-            selectors: [
-                { selector: selectSessionRefined, value: sessionRefined },
-                { selector: selectIsLoggedIn, value: false }
-            ]
+          initialState,
+          selectors: [
+            { selector: selectSessionData, value: sessionRefined },
+            { selector: selectIsLoggedIn, value: false }
+          ]
         }),
         { provide: SessionService, useValue: sessionServiceMock },
         { provide: window.sessionStorage, useClass: MockSessionStorageService },
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting()
-    ]
-});
+      ]
+    });
     mockStore = TestBed.inject(MockStore);
     clearSessionItems();
   });
 
   it('should save session without deleting session helpers', () => {
     // given
-    const sessionDto: SessionInfo = generateSessionDto();
+    const sessionDto: Session = generateSessionDto();
     setSessionItem('sessions', [sessionDto]);
-    const metaData: SessionMetaData = { mode: 'WIKI', label: 'coffee', option: 'SEARCH' };
+    const metaData: SessionMetaData = {
+      mode: SessionMode.Wiki,
+      label: 'coffee',
+      option: SessionOption.Search
+    };
     const actions$ = of(sessionActions.uploadOrSave(metaData));
 
     // when
@@ -54,24 +59,22 @@ describe('session effects', () => {
 
     // then
     expect(sessionServiceMock.uploadSessions).not.toHaveBeenCalled();
-    const items: Array<SessionInfo> | null = getSessionItem('sessions');
+    const items: Array<Session> | null = getSessionItem('sessions');
     expect(items?.length).toBe(2);
     expect(items).toEqual([{ ...sessionDto }, { ...sessionRefined, ...metaData }]);
   });
 
   it('should save session if upload error', () => {
     // given
-    const metaData: SessionMetaData = { mode: 'WIKI', label: 'coffee', option: 'SEARCH' };
+    const metaData: SessionMetaData = { mode: SessionMode.Wiki, label: 'coffee', option: SessionOption.Search };
     const actions$ = of(sessionActions.uploadOrSave(metaData));
-    sessionServiceMock.uploadSessions.mockImplementation(() =>
-      throwError(() => new Error('upload session error'))
-    );
+    sessionServiceMock.uploadSessions.mockImplementation(() => throwError(() => new Error('upload session error')));
 
     // when
     sessionUploadOrSave(actions$, sessionServiceMock as unknown as SessionService, mockStore).subscribe();
 
     // then
-    const items: Array<SessionInfo> | null = getSessionItem('sessions');
+    const items: Array<Session> | null = getSessionItem('sessions');
     expect(items).not.toBe(null);
     expect(items!.length).toBe(1);
     expect(items!.at(0)).toEqual({ ...sessionRefined, ...metaData });
@@ -79,7 +82,7 @@ describe('session effects', () => {
 
   it('should clean session helpers after uploadAll', () => {
     // given
-    const sessionDtos: Array<SessionInfo> = [generateSessionDto(), generateSessionDto()];
+    const sessionDtos: Array<Session> = [generateSessionDto(), generateSessionDto()];
     setSessionItem('sessions', sessionDtos);
     const actions$ = of(sessionActions.uploadAllSaved());
     const mockSessionService = {
@@ -90,7 +93,7 @@ describe('session effects', () => {
     sessionUploadAllSaved(actions$, mockSessionService as unknown as SessionService).subscribe();
 
     // then
-    const items: Array<SessionInfo> | null = getSessionItem('sessions');
+    const items: Array<Session> | null = getSessionItem('sessions');
     expect(items).toBeNull();
   });
 });
