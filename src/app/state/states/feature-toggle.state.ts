f7e22092618551cb39f4076d7cb3c39dfb3049ce
Florian Hamel <fhamel.freelance@gmail.com>
2025-07-06 00:42:31 +0200

    ✨ implemented state for feature toggles - not yet plugged to the API, the values have to be changed manually in the initState

---
+++ src/app/state/states/feature-toggle.state.ts
@@ -0,0 +1,9 @@
+export type FeatureToggleState = {
+  wikiRelatedToggle: false;
+  darkLightToggle: false;
+};
+
+export const initialState: FeatureToggleState = {
+  wikiRelatedToggle: false,
+  darkLightToggle: false
+};
